cdbf92858e157ceb5ff74434b6d3250788d08301
codeurabou <codeurabou123@gmail.com>
2024-12-09 22:22:34 +0000

    fix : fix app error sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 import "../styles/globals.css";
@@ -13,7 +13,8 @@ export default function GlobalError({
   reset: () => void;
 }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Sentry.captureException(error);
+    console.error("APP ERROR " + error?.digest || error);
   }, [error]);
 
   return (
